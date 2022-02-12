e189e2fc94de523eb28a6bf39a951d99f551e6b4
Jalal Rezeqat <47989412+jalalrezeqat@users.noreply.github.com>
2022-02-12 17:09:42 +0200

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table(table :'Users')->insert([
+            //Admin
+        [
+            'full_name'=>'prajwal Admin',
+            'username'=>'Admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make(value:'1111'),
+            'role'=>'admin',
+            'status'=>'active',
+
+        ],
+        //Vendor
+        [
+            'full_name'=>'prajwal Vendor',
+            'username'=>'Vendor',
+            'email'=>'vendor@gmail.com',
+            'password'=>Hash::make(value:'1111'),
+            'role'=>'vendor',
+            'status'=>'active',
+
+        ],
+        //customer
+        [
+            'full_name'=>'prajwal Customer',
+            'username'=>'Customer',
+            'email'=>'customer@gmail.com',
+            'password'=>Hash::make(value:'1111'),
+            'role'=>'customer',
+            'status'=>'active',
+
+        ],
+
+    ]);
+    }
+}
